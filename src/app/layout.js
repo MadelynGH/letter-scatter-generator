14b64c55346203caf9282ff316c60605f5a4f153
MadelynGH <91385745+MadelynGH@users.noreply.github.com>
2025-09-12 17:03:59 -0400

    Now has a basic Next.js app with a title of "Letter Scatter Generator"

---
+++ src/app/layout.js
@@ -0,0 +1,13 @@
+export const metadata = {
+    "title": "Letter Scatter Generator"
+}
+
+export default function RootLayout({ children }) {
+    return (
+        <html>
+            <body>
+                {children}
+            </body>
+        </html>
+    );
+}
